92ac221429d0a2d104133257305ef77511dcd39c
fatehhundal <fshundal@hotmail.com>
2026-06-06 12:28:59 +0800

    leapYear.java

---
+++ leapYear.java
@@ -0,0 +1,8 @@
+public class leapYear {
+    public static void main(String[] args) {
+        int year1 = 2024;
+        int year2 = 2026;
+        System.out.println((year1 % 4 == 0) ? "Leap Year":"Not a Leap Year");
+        System.out.println((year2 % 4 == 0) ? "Leap Year":"Not a Leap Year");
+    }
+}
