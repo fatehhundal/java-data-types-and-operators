3b7d66f72b520039de3292b02d2efad05f0246b4
fatehhundal <fshundal@hotmail.com>
2026-06-06 12:25:16 +0800

    methods.java

---
+++ methods.java
@@ -0,0 +1,13 @@
+public class methods {
+    public static void main(String[] args) {
+        System.out.println("My Maggi Recipe");
+        Maggi();
+    }
+
+    public static void Maggi() {
+        System.out.println("1. Boil water in pan.");
+        System.out.println("2. Add the Maggi masala into the water. You should be putting one packet per noodle cake.");
+        System.out.println("3. Add the noodles, and let them boil for two minutes or until they are fully cooked.");
+        System.out.println("4. Add the noodles to a serving dish and enjoy.");
+    }
+}
